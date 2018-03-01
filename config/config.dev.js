b9f98a9af98fafa9ba45e057d2806f2efd949299
Abhishek Saha <abhisheksaha11@gmail.com>
2018-03-02 00:04:25 +0100

    config change

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,4 +1,6 @@
 export default {
+    appPort: 4040,
+    apiPort: 3030,
     apiUrl: "http://localhost:3030/graphql",
     uploadUrl: "http://localhost:3030/upload",
     rootUrl: "http://localhost:4040/"
--- config/config.prod.js
+++ config/config.prod.js
@@ -1,10 +1,7 @@
-// export default {
-//     apiUrl: "http://api.letterpad.org/graphql",
-//     uploadUrl: "http://api.letterpad.org/upload",
-//     rootUrl: "http://letterpad.org/"
-// };
 export default {
-    apiUrl: "http://localhost:3030/graphql",
-    uploadUrl: "http://localhost:3030/upload",
-    rootUrl: "http://localhost:4040/"
+    apiUrl: "http://api.letterpad.org/graphql",
+    uploadUrl: "http://api.letterpad.org/upload",
+    rootUrl: "http://letterpad.org/",
+    appPort: 6060,
+    apiPort: 6061
 };
