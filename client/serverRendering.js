b5f7bb193b097ccb35f837c6375db9f717e35636
Abhishek Saha <abhisheksaha11@gmail.com>
2018-03-10 13:42:02 +0100

    added css file save

--- client/serverRendering.js
+++ client/serverRendering.js
@@ -89,6 +89,7 @@ function getHtml(content, state, head) {
                 />
                 ${metaTags}
                 <link href="/css/client.css" rel="stylesheet"/>
+                <link href="/css/custom.css" rel="stylesheet"/>
             </head>
             <body>
                 <div id="app">${content}</div>
