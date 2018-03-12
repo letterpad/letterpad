03ad38db813c24bea2fb3f270652c01382876a1d
Abhishek Saha <abhisheksaha11@gmail.com>
2018-03-12 19:49:11 +0100

    fixed layout

--- client/layouts/CenteredLayout.js
+++ client/layouts/CenteredLayout.js
@@ -13,8 +13,7 @@ export default class extends Component {
                     <div className="container">
                         <Navbar
                             settings={settings}
-                            menu={JSON.parse(settings.menu.value)}
-                            layout={settings.layout_display.value}
+                            position="top"
                             router={{ ...this.props }}
                         />
 
@@ -30,53 +29,3 @@ export default class extends Component {
         );
     }
 }
-
-// export default function Layout(Element) {
-//     return class extends Component {
-//         render() {
-//             const pageName = Element.type
-//                 ? Element.type.name.toLowerCase()
-//                 : "";
-//             const enhancedElement = React.cloneElement(Element, {
-//                 ...this.props
-//             });
-//             const { settings } = enhancedElement.props;
-//             const layout = settings.layout_display.value;
-//             let classes = {
-//                 navbarType:
-//                     layout == "centered"
-//                         ? "navbar navbar-default"
-//                         : "navbar navbar-custom"
-//             };
-//             return (
-//                 <div className={"main " + layout}>
-//                     <nav className={classes.navbarType}>
-//                         <div
-//                             className={
-//                                 layout == "two-column" ? "sidebar" : "container"
-//                             }
-//                         >
-//                             <Navbar
-//                                 settings={settings}
-//                                 menu={JSON.parse(settings.menu.value)}
-//                                 layout={layout}
-//                                 router={{ ...this.props }}
-//                             />
-
-//                             {layout == "two-column" ? (
-//                                 <Footer data={settings.site_footer.value} />
-//                             ) : (
-//                                 ""
-//                             )}
-//                         </div>
-//                     </nav>
-
-//                     <main>{enhancedElement}</main>
-//                     <aside>
-//                         <Sidebar settings={settings} {...this.props} />
-//                     </aside>
-//                 </div>
-//             );
-//         }
-//     };
-// }
