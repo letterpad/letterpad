9e50d8d90440ec9c8f65c57912223b386241364e
Abhishek Saha <abhisheksaha11@gmail.com>
2018-04-07 15:33:05 +0200

    fixed env file

--- .circleci/buildEnv.js
+++ .circleci/buildEnv.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 
 fs
     .createReadStream(__dirname + "/../sample.env")
-    .pipe(fs.createWriteStream(__dirname + "/../.env1"));
+    .pipe(fs.createWriteStream(__dirname + "/../.env"));
