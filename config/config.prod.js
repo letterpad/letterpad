d10cd9b10123788f7feb62bc1ae7601112e51ec8
Abhishek Saha <abhisheksaha11@gmail.com>
2018-04-22 22:10:32 +0200

    dont evaluate process.env in config.prod

--- config/config.prod.js
+++ config/config.prod.js
@@ -2,31 +2,31 @@ module.exports = {
     apiUrl: (function() {
         return typeof window !== "undefined"
             ? window.apiUrl
-            : process.env.apiUrl;
+            : eval("process.env.apiUrl");
     })(),
     uploadUrl: (function() {
         return typeof window !== "undefined"
             ? window.uploadUrl
-            : process.env.uploadUrl;
+            : eval("process.env.uploadUrl");
     })(),
     rootUrl: (function() {
         return typeof window !== "undefined"
             ? window.rootUrl
-            : process.env.rootUrl;
+            : eval("process.env.rootUrl");
     })(),
     appPort: (function() {
         return typeof window !== "undefined"
             ? window.appPort
-            : process.env.appPort;
+            : eval("process.env.appPort");
     })(),
     apiPort: (function() {
         return typeof window !== "undefined"
             ? window.apiPort
-            : process.env.apiPort;
+            : eval("process.env.apiPort");
     })(),
     baseName: (function() {
         return typeof window !== "undefined"
             ? window.baseName
-            : process.env.baseName;
+            : eval("process.env.baseName");
     })()
 };
