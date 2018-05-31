7707c52ca71aee27fe3de6394bdeba34f4473e1a
Abhishek Saha <abhisheksaha11@gmail.com>
2018-05-31 09:31:13 +0200

    fixed featured image

--- admin/components/Post/FeaturedImage.js
+++ admin/components/Post/FeaturedImage.js
@@ -57,7 +57,7 @@ class FeaturedImage extends Component {
     render() {
         const { t } = this.context;
         const coverImage =
-            this.state.cover_image || "http://placehold.it/800x400";
+            this.state.cover_image || "/images/placeholder-800x400.png";
 
         return (
             <div className="card">
