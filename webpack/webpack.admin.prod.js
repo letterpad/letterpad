5cc4eb687d55ef65afcf85e878066c19d8284402
Abhishek Saha <abhisheksaha11@gmail.com>
2018-06-13 10:50:30 +0200

    added admin build + seeding + image upload fix

---
+++ webpack/webpack.admin.prod.js
@@ -0,0 +1,47 @@
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const clientConfig = args => {
+    const extractPcssAdmin = new ExtractTextPlugin("[name].min.css");
+    if (typeof args === "undefined") {
+        args = { theme: "" };
+    }
+    const config = merge(baseConfig(args), {
+        target: "web",
+        output: {
+            path: path.join(__dirname, "../"),
+            filename: "[name]-bundle.min.js"
+        },
+        plugins: [extractPcssAdmin],
+        module: {
+            rules: [
+                {
+                    test: /\.(css|pcss)$/,
+                    use: extractPcssAdmin.extract({
+                        fallback: "style-loader",
+                        use: [
+                            {
+                                loader: "css-loader",
+                                options: { importLoaders: 1, minimize: true }
+                            },
+                            "postcss-loader"
+                        ]
+                    }),
+                    include: [
+                        path.resolve(__dirname, "../admin"),
+                        path.resolve(__dirname, "../node_modules"),
+                        path.resolve(__dirname, "../public")
+                    ]
+                }
+            ]
+        }
+    });
+    config.entry = {
+        "admin/public/dist/admin": [path.join(__dirname, "../admin/app")]
+    };
+    return config;
+};
+
+module.exports = args => [clientConfig(args)];
