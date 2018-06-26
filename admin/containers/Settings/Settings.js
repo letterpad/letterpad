65a299b10a1a85f2f70808599b55416294200d56
Tien Pham <tien.pham@metropolia.fi>
2018-06-26 09:22:06 +0300

    Refactor site settings in admin dashboard (#43)

--- admin/containers/Settings/Settings.js
+++ admin/containers/Settings/Settings.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { notify } from "react-notify-toast";
+import Loader from "admin/components/Loader";
+
 import {
     General,
     Social,
@@ -8,122 +12,138 @@ import {
     Messages
 } from "../../components/Settings";
 import { GET_OPTIONS } from "../../../shared/queries/Queries";
-import { notify } from "react-notify-toast";
 import UpdateOptions from "../../data-connectors/UpdateOptions";
+
+const Nav = styled.div`
+    display: flex;
+    margin-bottom: 3rem;
+    background: #f5f4f4;
+`;
+
+const NavItem = styled.div`
+    text-transform: capitalize;
+    user-select: none;
+    padding: 10px 15px;
+    border-bottom: ${p =>
+        p.active ? "2px solid black" : "2px solid transparent"};
+    cursor: pointer;
+    &:hover {
+        border-bottom: ${p =>
+            p.active ? "2px solid black" : "2px solid rgba(0,0,0, 0.3)"};
+    }
+`;
 
 const SubmitBtn = ({ handleClick }) => (
     <button type="submit" onClick={handleClick} className="btn btn-blue btn-sm">
         Save
     </button>
 );
+
 SubmitBtn.propTypes = {
     handleClick: PropTypes.func
+};
+
+const getPageComponent = (selected, data, updateOption) => {
+    switch (selected) {
+        case "general":
+            return <General data={data} updateOption={updateOption} />;
+        case "social":
+            return <Social data={data} updateOption={updateOption} />;
+        case "optional":
+            return (
+                <AdditionalSettings data={data} updateOption={updateOption} />
+            );
+        case "messages":
+            return <Messages data={data} updateOption={updateOption} />;
+        default:
+            return null;
+    }
 };
 
 class Settings extends Component {
     static propTypes = {
         updateOptions: PropTypes.func,
-        options: PropTypes.object
+        options: PropTypes.object,
+        history: PropTypes.object
     };
 
     static contextTypes = {
         t: PropTypes.func
     };
 
-    constructor(props) {
-        super(props);
-        this.updatedOptions = {};
-        this.submitData = this.submitData.bind(this);
-        this.setOption = this.setOption.bind(this);
-        document.body.classList.add("settings-page");
-    }
-    componentDidMount() {
-        const elem = document.querySelector(".masonry-grid");
-        setTimeout(() => {
-            new Masonry(elem, {
-                itemSelector: ".masonry-grid-item",
-                gutter: 10
-            });
-        }, 300);
-    }
+    state = {
+        updatedOptions: {},
+        selected:
+            new URLSearchParams(this.props.history.location.search).get(
+                "tab"
+            ) || "general"
+    };
 
-    componentWillUnmount() {
-        document.body.classList.remove("settings-page");
-    }
+    setOption = (option, value) => {
+        const { updatedOptions } = this.state;
+        updatedOptions[option] = value;
+    };
 
-    setOption(option, value) {
-        this.updatedOptions[option] = value;
-    }
-
-    submitData(e) {
+    submitData = e => {
         e.preventDefault();
         const settings = [];
-        Object.keys(this.updatedOptions).forEach(option => {
+        Object.keys(this.state.updatedOptions).forEach(option => {
             settings.push({
                 option,
-                value: this.updatedOptions[option]
+                value: this.state.updatedOptions[option]
             });
         });
+
         this.props.updateOptions(settings).then(() => {
             notify.show("Site settings saved", "success", 3000);
         });
-    }
+    };
+
+    handleNavClick = (page, e) => {
+        e.preventDefault();
+        this.props.history.push({
+            pathname: this.props.history.location.pathname,
+            search: "?tab=" + page
+        });
+    };
+
     render() {
+        const { selected } = this.state;
+        const { options } = this.props;
         const data = {};
         const { t } = this.context;
-        if (this.props.options.loading) {
-            return <div>hello</div>;
+        if (options.loading) {
+            return <Loader />;
         }
-        this.props.options.settings.forEach(setting => {
+        options.settings.forEach(setting => {
             data[setting.option] = setting;
         });
 
         return (
             <section className="module-xs">
-                <div className="masonry-grid">
-                    <div className="card masonry-grid-item">
-                        <div className="module-title">
-                            {t("settings.general.title")}
-                        </div>
-                        <div className="module-subtitle">
-                            {t("settings.general.tagline")}
-                        </div>
-                        <General data={data} updateOption={this.setOption} />
-                        <SubmitBtn handleClick={this.submitData} />
+                <div className="card">
+                    <Nav>
+                        {["general", "social", "optional", "messages"].map(
+                            (page, i) => (
+                                <NavItem
+                                    key={i}
+                                    active={selected === page}
+                                    onClick={e => this.handleNavClick(page, e)}
+                                >
+                                    {page}
+                                </NavItem>
+                            )
+                        )}
+                    </Nav>
+                    <div className="module-title">
+                        {t(`settings.${selected}.title`)}
                     </div>
-                    <div className="card masonry-grid-item">
-                        <div className="module-title">
-                            {t("settings.social.title")}
-                        </div>
-                        <div className="module-subtitle">
-                            {t("settings.social.tagline")}
-                        </div>
-                        <Social data={data} updateOption={this.setOption} />
-                        <SubmitBtn handleClick={this.submitData} />
+                    <div className="module-subtitle">
+                        {t(`settings.${selected}.tagline`)}
                     </div>
-                    <div className="card masonry-grid-item">
-                        <div className="module-title">
-                            {t("settings.additional.title")}
-                        </div>
-                        <div className="module-subtitle">
-                            {t("settings.additional.tagline")}
-                        </div>
-                        <AdditionalSettings
-                            data={data}
-                            updateOption={this.setOption}
-                        />
-                        <SubmitBtn handleClick={this.submitData} />
-                    </div>
-                    <div className="card masonry-grid-item">
-                        <div className="module-title">
-                            {t("settings.messages.title")}
-                        </div>
-                        <div className="module-subtitle">
-                            {t("settings.messages.tagline")}
-                        </div>
-                        <Messages data={data} updateOption={this.setOption} />
-                        <SubmitBtn handleClick={this.submitData} />
-                    </div>
+
+                    {getPageComponent(selected, data, this.setOption)}
+                    <SubmitBtn handleClick={this.submitData} />
                 </div>
             </section>
         );
