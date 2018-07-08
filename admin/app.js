59c0950034ef08b4f753f59a94c306cbea764a1f
Abhishek Saha <abhisheksaha11@gmail.com>
2018-07-09 01:15:18 +0200

    Removed console errors (#69)

--- admin/app.js
+++ admin/app.js
@@ -2,12 +2,12 @@
   Import Dependencies
 */
 import React from "react";
-import { hydrate } from "react-dom";
+import { render } from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter } from "react-router-dom";
 import client from "../shared/apolloClient";
 import Routes from "./Routes";
-import { Switch } from "react-router-dom";
+// import { Switch } from "react-router-dom";
 import "isomorphic-fetch";
 import config from "../config";
 
@@ -16,11 +16,9 @@ const isAdmin = true;
 const App = (
     <BrowserRouter basename={config.baseName}>
         <ApolloProvider client={client(isAdmin)}>
-            <Switch>
-                <Routes />
-            </Switch>
+            <Routes />
         </ApolloProvider>
     </BrowserRouter>
 );
 
-hydrate(App, document.getElementById("app"));
+render(App, document.getElementById("app"));
