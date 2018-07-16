565a750d34b1132d15b1ee14faa6705fff6889ab
Abhishek Saha <abhisheksaha11@gmail.com>
2018-07-16 18:11:48 +0200

    [Create Post] - minor fixes (#74)

--- shared/queries/Mutations.js
+++ shared/queries/Mutations.js
@@ -1,8 +1,8 @@
 const gql = require("graphql-tag");
 
 module.exports.CREATE_POST = gql`
-    mutation createPost($type: String!, $title: String!, $body: String) {
-        createPost(type: $type, title: $title, body: $body) {
+    mutation createPost($type: String!) {
+        createPost(type: $type) {
             ok
             errors {
                 path
