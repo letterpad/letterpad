9d970fb00eee38b7e0a70fdcd0fb02e5ba966790
Abhishek Saha <abhisheksaha11@gmail.com>
2018-08-06 16:56:51 +0200

    changed db name back to docs

--- config/db.config.js
+++ config/db.config.js
@@ -8,10 +8,10 @@ module.exports = {
     dev: {
         username: "root",
         password: null,
-        database: "letterpad_docs",
+        database: "letterpad_demo",
         host: "127.0.0.1",
         dialect: "sqlite",
-        storage: getFile("letterpad_docs"),
+        storage: getFile("letterpad_demo"),
         logging: false,
         define: {
             underscored: true
