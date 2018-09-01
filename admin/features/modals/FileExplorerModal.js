bb35ad49794f0e8effe8f86cffeff105696b909e
Abhishek Saha <abhisheksaha11@gmail.com>
2018-09-02 00:41:16 +0200

    [fix] - image paths

--- admin/features/modals/FileExplorerModal.js
+++ admin/features/modals/FileExplorerModal.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { translate } from "react-i18next";
-
+import styled from "styled-components";
 import ModalHoc from "../../components/modal";
 import StyledButton from "../../components/button";
 import FileExplorer from "../file-explorer";
+
+const StyledBody = styled.div`
+    .grid {
+        height: calc(100vh - 248px);
+    }
+`;
 
 class FileExplorerModal extends Component {
     state = {
@@ -30,7 +36,7 @@ class FileExplorerModal extends Component {
                 title={t("modal.explorer.title")}
                 onClose={this.props.onClose}
             >
-                <div className="modal-body text-center">
+                <StyledBody className="modal-body text-center">
                     <FileExplorer
                         author={{ id: 1 }}
                         page={this.state.page}
@@ -38,7 +44,7 @@ class FileExplorerModal extends Component {
                         onSelect={this.onSelect}
                     />
                     <div className="p-t-20" />
-                </div>
+                </StyledBody>
                 <div className="modal-footer">
                     <StyledButton onClick={this.props.onClose}>
                         {t("common.cancel")}
