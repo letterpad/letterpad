d3e4c948f65f9c2ec848add035b223a3f375718d
Abhishek Saha <abhisheksaha11@gmail.com>
2018-10-27 23:56:06 +0300

    Save css from admin panel (#96)

--- api/seed/index.js
+++ api/seed/index.js
@@ -1,5 +1,8 @@
-require("babel-register");
 require("@babel/polyfill");
+require("@babel/register")({
+    plugins: ["@babel/plugin-syntax-dynamic-import"],
+    presets: [["@babel/env"]]
+});
 var env = require("node-env-file");
 env(__dirname + "/../../.env");
 const models = require("../../api/models/index.js");
