09abcb2ff52e322e445ff4842b6926842ae839fd
Abhishek Saha <abhisheksaha11@gmail.com>
2019-01-14 03:15:17 +0100

    removed unused

--- admin/features/header/index.js
+++ admin/features/header/index.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import User from "./User";
-import config from "../../../config";
 import StyledHeader from "./Header.css";
 import StyledLink from "../../components/link";
 
