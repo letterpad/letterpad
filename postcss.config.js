bcc050100a087a75c9f3b5c5413486eff1dcae1c
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2019-05-30 01:25:09 +0200

    Updated letterpad-editor version and removed unused dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     "postcss-nested": {},
-    "postcss-cssnext": {
+    "postcss-preset-env": {
       browsers: ["last 2 versions", "> 5%"],
     },
   },
