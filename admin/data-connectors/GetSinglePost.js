7568a86b6e070a37e9327cde6fd5e6025fc70178
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2019-05-30 01:25:09 +0200

    Updated letterpad-editor version and removed unused dependencies

--- admin/data-connectors/GetSinglePost.js
+++ admin/data-connectors/GetSinglePost.js
@@ -2,7 +2,9 @@ import { graphql } from "react-apollo";
 import { GET_SINGLE_POST } from "../../shared/queries/Queries";
 
 export default graphql(GET_SINGLE_POST, {
-  options: props => ({ variables: { id: props.match.params.post_id } }),
+  options: props => ({
+    variables: { id: parseInt(props.match.params.post_id) },
+  }),
   props: ({ data: { loading, post } }) => ({
     post,
     loading,
