a9466f6a6ddaced6d24d3ae524c820112ca7c66c
Abhishek Saha <abhisheksaha11@gmail.com>
2019-09-17 16:01:57 +0200

    cleanup

--- utils/preinstall.sh
+++ utils/preinstall.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-QUIT=false
+set -e
 printf "$npm_execpath" | grep -q "yarn\.js$" || 
 (printf "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 \n⚠️ Use yarn not npm! ⚠️\n
-~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~" && QUIT=true)
-if [ $QUIT ]; then
-    printf "\n\n\n"
-    exit 1
-fi
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~" && exit 1)
+
 echo "Installing the default theme - Hugo"
 if [ ! -d client/themes/hugo ]; then
     git clone https://github.com/letterpad/theme-hugo.git client/themes/hugo --depth 1
