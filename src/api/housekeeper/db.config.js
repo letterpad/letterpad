7f75a92043a3630901de2932763307af31bbc068
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2020-02-01 21:20:03 +0100

    finished production build for preview (#141)

--- src/api/housekeeper/db.config.js
+++ src/api/housekeeper/db.config.js
@@ -1,7 +1,9 @@
 const env = require("node-env-file");
 env(__dirname + "/../../../.env");
 
-const configs = require("../../config/db.config.ts");
+const path = require("path");
+
+const configs = getConfigs();
 let config;
 
 if (Object.keys(configs).includes(process.env.NODE_ENV)) {
@@ -11,3 +13,54 @@ if (Object.keys(configs).includes(process.env.NODE_ENV)) {
 }
 console.log("This file was called from housekeeper");
 module.exports = config;
+
+//! This part is duplicated. This already exist in src/config/dbConfig.ts.
+//! But I am not able to import it here.
+function getConfigs() {
+  const getFile = name => {
+    const dbFile = name + ".sqlite";
+    return path.join(__dirname, "../../data/", dbFile);
+  };
+
+  const configs = {
+    dev: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_TYPE,
+      storage: getFile("letterpad"),
+      logging: false,
+      define: {
+        underscored: true,
+        raw: true,
+      },
+    },
+    test: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_TYPE,
+      storage: getFile("test"),
+      logging: false,
+      define: {
+        underscored: true,
+      },
+    },
+    production: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_TYPE,
+      storage: getFile("letterpad"),
+      logging: false,
+      define: {
+        underscored: true,
+      },
+    },
+  };
+
+  return configs;
+}
