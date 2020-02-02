df35a06e17afbe187b710b2895e263dc0fa56462
Abhishek Saha <abhisheksaha11@gmail.com>
2020-02-02 20:15:00 +0100

    clean up

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -69,7 +69,6 @@ module.exports = (args, name) => {
         "process.env": {
           apiUrl: "process.env.apiUrl",
           uploadUrl: "process.env.uploadUrl",
-          rootUrl: "",
           appPort: "process.env.appPort",
           baseName: "process.env.baseName",
         },
