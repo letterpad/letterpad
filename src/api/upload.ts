33a889beb2feb8b7a5775be913b69b9036353b16
Abhishek Saha <abhisheksaha11@gmail.com>
2020-02-07 13:02:54 +0100

    fix image upload

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,4 +1,5 @@
 import config from "../config";
+import fs from "fs";
 import logger from "../shared/logger";
 import path from "path";
 import resizeImage from "./utils/resizeImage";
@@ -14,6 +15,12 @@ interface IResultItem {
   name: string;
 }
 
+interface IFile {
+  mv: (path: string, callback: () => void) => Promise<IResultItem>;
+  md5: string;
+  name: string;
+}
+
 export default async (req, res) => {
   let files = req.files.file;
 
@@ -26,30 +33,59 @@ export default async (req, res) => {
   const output: IResultItem[] = [];
 
   for (const file of files) {
-    const { md5, name } = file;
-    const extension = name.substr(name.lastIndexOf(".") + 1);
-    const resultItem: IResultItem = { src: "", error: null, name };
     if (file) {
-      let filename = md5 + "." + extension;
       try {
-        await resizeImage({
-          type: req.body.type,
-          ext: extension,
-          tempPath: file.tempFilePath,
-          uploadDir: uploadDir + filename,
-        });
-        await models.default.Media.create({
-          AuthorId: req.user.id,
-          url: "/uploads/" + filename,
-        });
-        resultItem.src = host + "/uploads/" + filename;
+        const result = await resizeAndSave(file, uploadDir, req.user.id);
+        output.push(result);
       } catch (e) {
-        logger.error(e, resultItem);
-        resultItem.src = "";
-        resultItem.error = e;
+        logger.error(e);
+        output.push(e);
       }
     }
-    output.push(resultItem);
   }
   res.json(output);
 };
+
+function resizeAndSave(
+  file: IFile,
+  uploadDir: string,
+  id: number,
+): Promise<IResultItem> {
+  return new Promise((resolve, reject) => {
+    // we have received the file through the express-fileupload middleware
+    const { md5, name, mv } = file;
+
+    const extension = name.substr(name.lastIndexOf(".") + 1);
+    const filename = md5 + "." + extension;
+    const uploadPath = uploadDir + filename;
+    const tempPath = uploadDir + "temp." + filename;
+
+    const resultItem: IResultItem = { src: "", error: null, name };
+
+    // move the file to a temp path and apply resizer on it
+    mv(tempPath, async () => {
+      try {
+        await resizeImage({
+          ext: extension,
+          tempPath: tempPath,
+          uploadDir: uploadPath,
+        });
+        // remove the temp file
+        fs.unlinkSync(tempPath);
+        await models.default.Media.create({
+          AuthorId: id,
+          url: "/uploads/" + filename,
+        });
+        resultItem.src = host + "/uploads/" + filename;
+        resolve(resultItem);
+      } catch (e) {
+        if (fs.existsSync(tempPath)) {
+          fs.unlinkSync(tempPath);
+        }
+        logger.error(e, resultItem);
+        resultItem.error = e;
+        reject(resultItem);
+      }
+    });
+  });
+}
