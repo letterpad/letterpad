ea1a66719648702a8d214119495324cf089e5a00
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2020-02-07 01:49:23 +0100

    fixed transparent image uploads like logo (#153)

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -5,7 +5,6 @@ import bodyParser from "body-parser";
 import constants from "./utils/constants";
 import cors from "cors";
 
-const fileUpload = require("express-fileupload");
 const jwt = require("jsonwebtoken");
 const corsMiddleWare = cors({
   exposedHeaders: ["x-refresh-token"],
@@ -50,20 +49,10 @@ const addRefreshToken = async (
   next();
 };
 
-const MAX_UPLOAD_SIZE = parseInt(process.env["MAX_IMAGE_UPLOAD_SIZE"] || "10");
-
 export default function(app: Express) {
   app.use(corsMiddleWare);
   app.options("*", cors());
   app.use(bodyParserMiddleWare);
   app.use(bodyParser.json());
   app.use(addRefreshToken);
-  app.use(
-    fileUpload({
-      limits: { fileSize: MAX_UPLOAD_SIZE * 1024 * 1024 },
-      abortOnLimit: true,
-      useTempFiles: true,
-      tempFileDir: "/tmp/",
-    }),
-  );
 }
