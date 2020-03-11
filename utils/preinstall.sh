6e3c507d58d7b69e0bc0e057437e73b86debec2d
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2020-03-11 16:53:20 +0100

    Refactoring url providers (#165)

--- utils/preinstall.sh
+++ utils/preinstall.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+printf "$npm_execpath" | grep -q "yarn\.js$" || 	
+(printf "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~	
+\n⚠️ Please use yarn, NOT npm! \n 
+ Letterpad uses workspaces feature which is not available in npm yet. \n	
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n" && exit 1)
+
 if [ ! -f src/client/themes/hugo/package.json ]; then
     echo "Installing the default theme - Hugo"
     git clone https://github.com/letterpad/theme-hugo.git src/client/themes/hugo --depth 1
