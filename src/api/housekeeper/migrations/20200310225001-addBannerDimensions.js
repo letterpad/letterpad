f61e145167ea3c485efc1f1019bb5877f174e816
Abhishek Saha <abhisheksaha11@gmail.com>
2020-03-12 16:09:10 +0100

    fix setting migration

--- src/api/housekeeper/migrations/20200310225001-addBannerDimensions.js
+++ src/api/housekeeper/migrations/20200310225001-addBannerDimensions.js
@@ -5,25 +5,41 @@ module.exports = {
     const src = await queryInterface.sequelize.query(
       "SELECT value from setting WHERE option='banner'",
     );
+    let value = src;
+    try {
+      // value is already json
+      value = JSON.parse(value);
+    } catch (e) {
+      value = JSON.stringify({
+        src: src[0][0].value,
+        width: 100,
+        height: 100,
+      });
 
-    const value = JSON.stringify({
-      src: src[0][0].value,
-      width: 100,
-      height: 100,
-    });
-    console.log("value :", value);
-    return queryInterface.sequelize.query(
-      `
-      UPDATE setting SET value="${value}" WHERE option="banner"
-      `,
-    );
+      return queryInterface.sequelize.query(
+        // prettier-ignore
+        // eslint-disable-next-line quotes
+        "UPDATE setting SET `value`='" + value + "' WHERE `option`=\"banner\"",
+        { logging: console.log },
+      );
+    }
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.update("setting", [
-      {
-        option: "site_favicon",
-      },
-    ]);
+  down: async (queryInterface, Sequelize) => {
+    const src = await queryInterface.sequelize.query(
+      "SELECT value from setting WHERE option='banner'",
+    );
+    let value = src;
+    try {
+      value = JSON.parse(value);
+      return queryInterface.sequelize.query(
+        // prettier-ignore
+        // eslint-disable-next-line quotes
+        "UPDATE setting SET `value`='" + value.src + "' WHERE `option`=\"banner\"",
+        { logging: console.log },
+      );
+    } catch (e) {
+      //
+    }
   },
 };
