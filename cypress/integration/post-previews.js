5d164db0c240e970b07e6876d95804ce1bcdbc02
Abhishek Saha <1502352+ajaxtown@users.noreply.github.com>
2020-04-25 16:03:25 +0200

    Add new cypress test - preview post in draft and republish states (#193)

---
+++ cypress/integration/post-previews.js
@@ -0,0 +1,72 @@
+describe("Preview", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it("draft preview should work", () => {
+    cy.get("a[href*='/admin/post-new']").click();
+
+    // write title and body
+    cy.get(".post-header div")
+      .type("Draft post preview - title")
+      .tab()
+      .type("Draft post preview - body");
+
+    cy.wait(1000);
+
+    // open settings
+    cy.get("button")
+      .contains("Publish")
+      .click();
+
+    cy.wait(1000);
+    cy.get("[data-testid='link-preview']")
+      .should("have.attr", "href")
+      .then(src => {
+        cy.visit("http://localhost:4040" + src);
+        cy.contains("Draft post preview - title");
+        cy.contains("Draft post preview - body");
+      });
+  });
+
+  it("republish preview should work", () => {
+    cy.get("a[href*='/admin/post-new']").click();
+
+    // write title and body
+    cy.get(".post-header div")
+      .type("Republish post preview - title")
+      .tab()
+      .type("Republish post preview - body");
+
+    cy.wait(1000);
+
+    // open settings
+    cy.get("button")
+      .contains("Publish")
+      .click();
+
+    // publish now
+    cy.get("button")
+      .contains("Publish Now")
+      .click();
+
+    cy.get("[data-testid='close-settings']").click();
+
+    // post is published now. change some stuff
+    // change body
+    cy.get(".post-header div")
+      .tab()
+      .type("new content added");
+
+    cy.wait(1000);
+
+    cy.get("[data-testid='cog-settings']").click();
+    cy.wait(1000);
+    cy.get("[data-testid='link-preview']")
+      .should("have.attr", "href")
+      .then(src => {
+        cy.visit("http://localhost:4040" + src);
+        cy.contains("new content added");
+      });
+  });
+});
