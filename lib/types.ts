6967843c3668fbda1d0b3ff81df07cdeed4fcf91
Abhishek Saha <abhisheksaha11@gmail.com>
2021-04-11 01:15:35 +0530

    improve queries schema

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,11 @@
+import { Author } from "./../__generated__/lib/type-defs.graphqls";
 import { Post } from "./type-defs.graphqls";
 
 export enum ROLES {
   ADMIN,
+  REVIEWER,
+  READER,
+  AUTHOR,
 }
 
 export enum PERMISSIOMS {
@@ -20,3 +24,8 @@ export type updatePostOptionalArgs = {
   cover_image_width?: number;
   cover_image_height?: number;
 } & Omit<Post, "cover_image" | "cover_image_width" | "cover_image_height">;
+
+export type SessionData = Pick<
+  Author,
+  "id" | "email" | "role" | "permissions" | "avatar"
+>;
