331a49147dad02d37e0d183044d61c62fa73e400
Abhishek Saha <abhisheksaha11@gmail.com>
2021-04-11 01:13:56 +0530

    fix graphql playground

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apollo";
+import { Provider } from "next-auth/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <Provider session={pageProps.session}>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </Provider>
   );
 }
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,17 +1,11 @@
-import { IncomingMessage } from "http";
 import { ApolloServer } from "apollo-server-micro";
-import { getSession } from "next-auth/client";
 import models from "../../db/models";
 import { schema } from "../../lib/schema";
 
 const apolloServer = new ApolloServer({
   schema,
-  context: async ({ req }: { req: IncomingMessage }) => {
-    const session = await getSession({ req });
-    if (session) {
-      console.log("session :>> ", session);
-    }
-    return { req, models, session };
+  context: async context => {
+    return { ...context, models };
   },
 });
 
