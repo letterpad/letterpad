6b5a6c5db5e33afab7008ed05202eeff97015303
Abhishek Saha <abhisheksaha11@gmail.com>
2021-06-30 02:33:35 +0200

    sentry test

---
+++ src/pages/api/sentry-test.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from "@sentry/nextjs";
+
+const handler = async (req, res) => {
+  throw new Error("API throw error for testing sentry");
+};
+
+export default withSentry(handler);
