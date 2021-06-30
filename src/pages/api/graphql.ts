76cb5f3c2b5c4ff0581efbc7d5e81846ac0e6521
Abhishek Saha <abhisheksaha11@gmail.com>
2021-06-30 15:18:14 +0200

    attach user to sentry

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -3,12 +3,14 @@ import { ApolloServer } from "apollo-server-micro";
 import models from "@/graphql/db/models";
 import { schema } from "@/graphql/schema";
 import getAuthorIdFromRequest from "src/shared/getAuthorIdFromRequest";
+import * as Sentry from "@sentry/nextjs";
 
 export const apolloServer = new ApolloServer({
   schema,
   context: async (context) => {
     const session = await getSession(context);
     const author_id = await getAuthorIdFromRequest({ req: context.req });
+    Sentry.setUser({ email: session?.user?.email || "" });
     return { ...context, models, author_id, session };
   },
   playground: true,
