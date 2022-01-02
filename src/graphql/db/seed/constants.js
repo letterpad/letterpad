3bd874f4cfd21d252ba8b233cd6e6755565eb536
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-01-02 14:46:34 +0100

    fix subjects

--- src/graphql/db/seed/constants.js
+++ src/graphql/db/seed/constants.js
@@ -4,7 +4,7 @@
 
 module.exports.subjects = {
   VERIFY_NEW_USER: "{{ blog_name }} - Verify Email",
-  VERIFY_NEW_SUBSCRIBER: "{{ blog_name }} - reset your password",
-  FORGOT_PASSWORD: "{{ blog_name }} - Verify your email",
+  FORGOT_PASSWORD: "{{ blog_name }} - reset your password",
+  VERIFY_NEW_SUBSCRIBER: "{{ blog_name }} - Verify your email",
   NEW_POST: "{{ blog_name }} - New Post",
 };
