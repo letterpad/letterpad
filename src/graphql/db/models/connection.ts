d4d995418525b1550ebc6b3174a4f63f2be1f1e6
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-01-20 22:51:20 +0100

    fixed

--- src/graphql/db/models/connection.ts
+++ src/graphql/db/models/connection.ts
@@ -1,5 +1,9 @@
 import dbConfig from "../../../../config/db.config";
-import { Sequelize } from "sequelize-typescript";
+/**
+ * ! Import didnt work. During build, it would throw this error:
+ * Attempted import error: 'Sequelize' is not exported from 'sequelize-typescript' (imported as 'Sequelize').
+ */
+const { Sequelize } = require("sequelize-typescript");
 
 enum envs {
   development = "development",
@@ -16,12 +20,7 @@ if (env === envs.test) env = envs.test;
 
 const config = dbConfig[env];
 // establish  database connection
-export const connection = new Sequelize(
-  config.database as string,
-  config.username as string,
-  config.password as string,
-  {
-    ...config,
-    dialect: config.dialect || "sqlite",
-  },
-);
+export const connection = new Sequelize({
+  ...config,
+  dialect: config.dialect || "sqlite",
+});
