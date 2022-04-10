05dba9e3e4ac746a8193f4b77aedcf2d88087d96
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-04-11 02:59:06 +0530

    Update: demo data (#330)

--- src/graphql/db/seed/constants.js
+++ src/graphql/db/seed/constants.js
@@ -29,10 +29,9 @@ module.exports.defaultSettings = {
   site_title: "Letterpad",
   site_tagline: "My space",
   site_email: "admin@letterpad.app",
-  site_url: "https://demo.letterpad.app",
-  site_footer: "Wonderful digital things with a pinch of rock and roll!",
-  site_description:
-    "Digital nomad creating content about building IT businesses and production-ready apps.",
+  site_url: "",
+  site_footer: "Letterpad is an open source project licensed under MIT.",
+  site_description: "Use this space to describe your blog.",
   subscribe_embed: "",
   social_twitter: "",
   social_facebook: "",
@@ -40,12 +39,12 @@ module.exports.defaultSettings = {
   social_github: "",
   display_author_info: true,
   site_logo: JSON.stringify({
-    src: "/uploads/logo.png",
+    src: "https://letterpad.app/admin/uploads/logo.png",
     width: 200,
     height: 200,
   }),
   site_favicon: JSON.stringify({
-    src: "/uploads/logo.png",
+    src: "https://letterpad.app/admin/uploads/logo.png",
     width: 200,
     height: 200,
   }),
