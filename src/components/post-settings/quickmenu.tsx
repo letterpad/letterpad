f91a9e6f8edf2a828165f2d069bf5c72fe329a91
Abhishek Saha <abhishek.saha@hometogo.com>
2022-09-10 20:08:16 +0200

    chore: clean up

--- src/components/post-settings/quickmenu.tsx
+++ src/components/post-settings/quickmenu.tsx
@@ -1,5 +1,5 @@
 import { EyeOutlined, SettingOutlined } from "@ant-design/icons";
-import { Button, Menu } from "antd";
+import { Button } from "antd";
 
 import { socket } from "@/components/post/components/tinymce/socket";
 
@@ -41,43 +41,6 @@ const QuickMenu = ({ siteUrl, postHash, showDrawer }) => {
         icon={<SettingOutlined />}
       ></Button>
     </>
-  );
-  return (
-    <Menu>
-      <Menu.Item key="0" onClick={showDrawer} data-testid="postSettingsLink">
-        Settings
-      </Menu.Item>
-      <Menu.Divider />
-      {siteUrl && (
-        <Menu.Item
-          key="2"
-          onClick={() => {
-            track({
-              eventAction: EventAction.Change,
-              eventCategory: "setting",
-              eventLabel: "preview",
-            });
-            window.open(siteUrl + "/preview/" + postHash);
-          }}
-        >
-          Preview
-        </Menu.Item>
-      )}
-      <Menu.Divider />
-      <Menu.Item
-        key="3"
-        onClick={() => {
-          track({
-            eventAction: EventAction.Click,
-            eventCategory: "post",
-            eventLabel: "grammar",
-          });
-          socket.checkGrammar();
-        }}
-      >
-        Check Grammar
-      </Menu.Item>
-    </Menu>
   );
 };
 export default QuickMenu;
