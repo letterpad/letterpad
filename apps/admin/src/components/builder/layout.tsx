1065aeec05921e58198b8758af4502eec10976b8
Abhishek Saha <abhishek.saha@hometogo.com>
2022-12-25 18:38:21 +0530

    chore: add fonts to head page-level creatives

--- apps/admin/src/components/builder/layout.tsx
+++ apps/admin/src/components/builder/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { FC } from "react";
 
 import { Row } from "@/components/builder/layouts/story-builder";
@@ -15,27 +16,41 @@ interface Props {
 export const Layout: FC<Props> = ({ editable = true }) => {
   const { grid } = useBuilderContext();
   return (
-    <div id="creative">
-      {editable && (
-        <div className="flex justify-end p-4">
-          <EditSwitch />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&family=Bowlby+One+SC&family=Bungee+Inline&family=Caveat:wght@500;700&family=Farsan&family=Germania+One&family=Knewave&family=Major+Mono+Display&family=Merriweather:ital,wght@0,400;0,700;0,900;1,700&family=Metal+Mania&family=Nanum+Pen+Script&family=Niconne&family=PT+Sans:ital,wght@0,400;0,700;1,400&family=Potta+One&family=Raleway:ital,wght@0,400;0,500;0,800;1,500&family=Roboto:wght@400;500;900&family=Skranji&family=Spectral:ital,wght@0,400;0,500;0,700;0,800;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div id="creative">
+        {editable && (
+          <div className="flex justify-end p-4">
+            <EditSwitch />
+          </div>
+        )}
+        <div className="flex flex-col">
+          {grid.map((row, rowIndex) => {
+            const isPrevRowImageLeft =
+              grid[rowIndex - 1]?.data[0]?.type === "image";
+            return (
+              <Row
+                isPrevRowImageLeft={isPrevRowImageLeft}
+                row={row}
+                key={row.id}
+                rowIndex={rowIndex}
+              />
+            );
+          })}
         </div>
-      )}
-      <div className="flex flex-col">
-        {grid.map((row, rowIndex) => {
-          const isPrevRowImageLeft =
-            grid[rowIndex - 1]?.data[0]?.type === "image";
-          return (
-            <Row
-              isPrevRowImageLeft={isPrevRowImageLeft}
-              row={row}
-              key={row.id}
-              rowIndex={rowIndex}
-            />
-          );
-        })}
       </div>
-    </div>
+    </>
   );
 };
 export default Layout;
--- apps/client/layouts/Creative.tsx
+++ apps/client/layouts/Creative.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import {
   MeFragment,
   PageFragmentFragment,
@@ -37,6 +39,18 @@ export default function Creative({ site_name, data, settings, me }: Props) {
   ];
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&family=Bowlby+One+SC&family=Bungee+Inline&family=Caveat:wght@500;700&family=Farsan&family=Germania+One&family=Knewave&family=Major+Mono+Display&family=Merriweather:ital,wght@0,400;0,700;0,900;1,700&family=Metal+Mania&family=Nanum+Pen+Script&family=Niconne&family=PT+Sans:ital,wght@0,400;0,700;1,400&family=Potta+One&family=Raleway:ital,wght@0,400;0,500;0,800;1,500&family=Roboto:wght@400;500;900&family=Skranji&family=Spectral:ital,wght@0,400;0,500;0,700;0,800;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <BlogSEO
         url={`${settings.site_url}${slug}`}
         date={publishedAt}
