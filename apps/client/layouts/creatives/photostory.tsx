7c7d3b2691909c735bc53e61c3e0cfa59214cbe3
Abhishek Saha <abhishek.saha@hometogo.com>
2022-12-26 14:55:57 +0530

    chore: fix cloudinary custom width

--- apps/client/layouts/creatives/photostory.tsx
+++ apps/client/layouts/creatives/photostory.tsx
@@ -133,7 +133,7 @@ const SectionText: FC<any> = ({ columns, text, type }) => {
       data-text
       className={
         'my-20 flex w-full flex-col items-center justify-center p-6 text-center leading-6 text-gray-800 dark:text-white ' +
-        (columns == 2 ? 'max-w-full lg:max-w-[calc(500px)]' : 'w-full ')
+        (columns == 2 ? ' max-w-full lg:max-w-[calc(500px)]' : ' w-full ')
       }
       dangerouslySetInnerHTML={{
         __html: decodeURIComponent(text),
