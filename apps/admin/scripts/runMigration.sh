6039c633f6feb5723e27ebc1c229b00b47a834f4
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-06 01:53:28 +0530

    chore: auto run migrations

--- apps/admin/scripts/runMigration.sh
+++ apps/admin/scripts/runMigration.sh
@@ -2,7 +2,7 @@
 
 source scripts/schema.sh
 
-if [ $NODE_ENV -eq "production" ]; then
+if [[ $NODE_ENV -eq "production" ]]; then
     npx prisma migrate deploy --schema prisma/$PRISMA_FILE
 else
     npx prisma migrate dev --schema prisma/$PRISMA_FILE
