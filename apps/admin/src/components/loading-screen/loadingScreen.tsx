dc24240759bb6bbb09d36ee6e1cacb403b8cad14
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-08 12:41:31 +0530

    chore: change loading screen

--- apps/admin/src/components/loading-screen/loadingScreen.tsx
+++ apps/admin/src/components/loading-screen/loadingScreen.tsx
@@ -1,10 +1,9 @@
 export const LoadingScreen = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75">
-      <div className="loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear"></div>
-      <h2 className="text-center text-xl font-semibold text-white">
-        Loading...
-      </h2>
+      <div className="absolute right-1/2 bottom-1/2  translate-x-1/2 translate-y-1/2 transform ">
+        <div className="h-20 w-20 animate-spin  rounded-full border-4 border-solid border-blue-400 border-t-transparent"></div>
+      </div>
     </div>
   );
 };
