88959c3adccd099eebe81975eb0ba0cec036d933
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-09 10:59:26 +0530

    chore: auth wallpaper revert

--- apps/admin/src/components/register/views/RegisterForm.tsx
+++ apps/admin/src/components/register/views/RegisterForm.tsx
@@ -110,10 +110,10 @@ export const RegisterForm = () => {
             className="hidden bg-cover lg:block lg:w-2/3"
             style={{
               backgroundImage:
-                "url(https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80)",
+                "url(https://images.unsplash.com/photo-1606625058344-64612c845754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
             }}
           >
-            <div className="flex h-full items-center bg-gray-900 bg-opacity-40 px-20">
+            <div className="flex h-full items-center bg-gray-900 bg-opacity-10 px-20">
               <div>
                 <h2 className="text-4xl font-bold text-white">Letterpad</h2>
 
