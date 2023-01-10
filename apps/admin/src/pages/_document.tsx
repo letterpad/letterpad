545954b83a7e45e405e724f8b54eb96054b878cd
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-11 01:34:51 +0530

    chore: point to GA4

--- apps/admin/src/pages/_document.tsx
+++ apps/admin/src/pages/_document.tsx
@@ -1,7 +1,8 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 import React from "react";
 
-import { basePath } from "@/constants";
+import { basePath, gaTrackingId } from "@/constants";
 
 class MyDocument extends Document {
   render() {
@@ -10,6 +11,19 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href={basePath + "/css/theme-variables.css"} />
           <script src={basePath + `/prism/prism.js`} async />
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${gaTrackingId});
+        `}
+          </Script>
         </Head>
         <body className="text-base">
           <Main />
