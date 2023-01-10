3983351ef2194a19f71b4d6e658fdc05e714b8f3
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-11 02:22:11 +0530

    fix: revert

--- apps/admin/src/pages/_document.tsx
+++ apps/admin/src/pages/_document.tsx
@@ -1,8 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 import React from "react";
 
-import { basePath, gaTrackingId } from "@/constants";
+import { basePath } from "@/constants";
 
 class MyDocument extends Document {
   render() {
@@ -11,19 +10,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href={basePath + "/css/theme-variables.css"} />
           <script src={basePath + `/prism/prism.js`} async />
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${gaTrackingId}');
-        `}
-          </Script>
         </Head>
         <body className="text-base">
           <Main />
