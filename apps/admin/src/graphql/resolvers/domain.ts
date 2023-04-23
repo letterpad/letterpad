4d9cd87b6b992679e318e336cee36b23d72dd75e
Abhishek Saha <abhishek.saha@hometogo.com>
2023-04-23 16:05:13 +0200

    chore: domain fallback to old

--- apps/admin/src/graphql/resolvers/domain.ts
+++ apps/admin/src/graphql/resolvers/domain.ts
@@ -25,57 +25,58 @@ const Mutation: Optional<MutationResolvers<ResolverContext>> = {
     return removeDomain(_args, context);
   },
   createOrUpdateDomain: async (_, args, context) => {
-    try {
-      if (!context.session?.user || !args.data.name) {
-        return {
-          ok: false,
-          message: "No session found",
-        };
-      }
-      const ssl = new SSL();
-      await ssl.add(args.data.name.trim());
-      const domainExist = await context.prisma.domain.findFirst({
-        where: {
-          author: {
-            id: context.session.user.id,
-          },
-        },
-      });
-      if (!domainExist) {
-        await context.prisma.domain.create({
-          data: {
-            name: args.data.name.trim(),
-            ssl: true,
-            mapped: true,
-            author: {
-              connect: {
-                id: context.session.user.id,
-              },
-            },
-          },
-        });
-      } else {
-        await context.prisma.domain.update({
-          data: {
-            ssl: true,
-            mapped: true,
-          },
-          where: {
-            author_id: context.session.user.id,
-          },
-        });
-      }
-      await enqueueEmailAndSend({
-        author_id: context.session.user.id,
-        template_id: EmailTemplates.DomainMapSuccess,
-      });
-      return {
-        ok: true,
-        message: "Congratulations! Your domain has been mapped with Letterpad",
-      };
-    } catch (e) {
-      return createOrUpdateDomain(args, context);
-    }
+    return createOrUpdateDomain(args, context);
+    // try {
+    //   if (!context.session?.user || !args.data.name) {
+    //     return {
+    //       ok: false,
+    //       message: "No session found",
+    //     };
+    //   }
+    //   const ssl = new SSL();
+    //   await ssl.add(args.data.name.trim());
+    //   const domainExist = await context.prisma.domain.findFirst({
+    //     where: {
+    //       author: {
+    //         id: context.session.user.id,
+    //       },
+    //     },
+    //   });
+    //   if (!domainExist) {
+    //     await context.prisma.domain.create({
+    //       data: {
+    //         name: args.data.name.trim(),
+    //         ssl: true,
+    //         mapped: true,
+    //         author: {
+    //           connect: {
+    //             id: context.session.user.id,
+    //           },
+    //         },
+    //       },
+    //     });
+    //   } else {
+    //     await context.prisma.domain.update({
+    //       data: {
+    //         ssl: true,
+    //         mapped: true,
+    //       },
+    //       where: {
+    //         author_id: context.session.user.id,
+    //       },
+    //     });
+    //   }
+    //   await enqueueEmailAndSend({
+    //     author_id: context.session.user.id,
+    //     template_id: EmailTemplates.DomainMapSuccess,
+    //   });
+    //   return {
+    //     ok: true,
+    //     message: "Congratulations! Your domain has been mapped with Letterpad",
+    //   };
+    // } catch (e) {
+    //   return createOrUpdateDomain(args, context);
+    // }
   },
 };
 
