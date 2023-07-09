079932b5fe8dda1bfd60edece6c19a5147c0a442
Abhishek Saha <abhishek.saha@hometogo.com>
2023-07-09 20:17:38 +0200

    chore: find cookies in serverless

--- apps/admin/src/pages/api/graphql.ts
+++ apps/admin/src/pages/api/graphql.ts
@@ -6,12 +6,24 @@ import { basePath } from "@/constants";
 import { getResolverContext } from "@/graphql/context";
 import { schema } from "@/graphql/schema";
 
+export const getServerSession = async (cookie: string) => {
+  const res = await fetch("/api/auth/session", {
+    headers: { cookie: cookie },
+  });
+  const session = await res.json();
+  // eslint-disable-next-line no-console
+  console.log("session", session);
+  return session;
+};
+
 export const apolloServer = new ApolloServer({
   schema,
   context: async (context) => {
     // eslint-disable-next-line no-console
+    console.log("base url", context.req.headers.origin);
+    // eslint-disable-next-line no-console
     console.log("req", context.req.cookies);
-
+    const session = await getServerSession(context.req.headers.cookie);
     const resolverContext = await getResolverContext(context);
     return resolverContext;
   },
