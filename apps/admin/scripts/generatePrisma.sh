8fc026b80d623edd439a586f69bfaf619ac6a293
Abhishek Saha <abhishek.saha@hometogo.com>
2023-07-26 23:01:01 +0200

    chore: add logging

--- apps/admin/scripts/generatePrisma.sh
+++ apps/admin/scripts/generatePrisma.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source scripts/schema.sh
-print("Generating Prisma Client:","prisma/$PRISMA_FILE")
-print("Generating Prisma Client - DB URL:", $DATABASE_URL)
+print "Generating Prisma Client:"
+print "prisma/$PRISMA_FILE"
+print "Generating Prisma Client - DB URL:"
+print "$DATABASE_URL"
 npx prisma generate --schema prisma/$PRISMA_FILE
