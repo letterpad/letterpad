40fee17d31dd30d06b52c310f7650222f1a15e11
Abhishek Saha <abhishek.saha@hometogo.com>
2023-07-28 13:59:21 +0200

    chore: console log folders

--- apps/admin/scripts/generatePrisma.sh
+++ apps/admin/scripts/generatePrisma.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 source scripts/schema.sh
-echo "Generating Prisma Client:"
-echo "prisma/$PRISMA_FILE"
-echo "Generating Prisma Client - DB URL:"
-echo "$DATABASE_URL"
+
 npx prisma generate --schema prisma/$PRISMA_FILE
