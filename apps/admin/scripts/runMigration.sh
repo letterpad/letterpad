ac308db584127a86c8586beefc8de37d4a9b0daf
Abhishek Saha <abhishek.saha@hometogo.com>
2023-07-29 20:51:10 +0200

    chore: fix dev installation

--- apps/admin/scripts/runMigration.sh
+++ apps/admin/scripts/runMigration.sh
@@ -2,6 +2,14 @@
 
 source scripts/schema.sh
 
+# for planetscale prisma migration is not required.
+DB_IGNORE_MIGRATION="connect.psdb"
+
+if [[ $DATABASE_URL == *"$DB_IGNORE_MIGRATION"* ]]; then
+    echo "Ignoring Prisma migration for PlanetScale DB"
+    exit 0
+fi
+
 if [[ $NODE_ENV -eq "production" ]]; then
     npx prisma migrate deploy --schema prisma/$PRISMA_FILE
 else
