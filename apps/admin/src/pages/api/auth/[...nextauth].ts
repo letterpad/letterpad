c38e019169ca0b85a0284f2ef16759a9305b3c3b
Abhishek Saha <abhishek.saha@hometogo.com>
2023-07-31 21:07:52 +0200

    chore: reset everything

--- apps/admin/src/pages/api/auth/[...nextauth].ts
+++ apps/admin/src/pages/api/auth/[...nextauth].ts
@@ -13,9 +13,9 @@ import { report } from "@/components/error";
 
 import { RegisterStep } from "@/__generated__/__types__";
 import { basePath } from "@/constants";
+import { getRootUrl } from "@/shared/getRootUrl";
 
 import { isBlackListed } from "./blacklist";
-// import { getRootUrl } from "@/shared/getRootUrl";
 
 const providers = (): NextAuthOptions["providers"] => [
   GoogleProvider({
@@ -80,16 +80,10 @@ const options = (): NextAuthOptions => ({
   providers: providers(),
   callbacks: {
     redirect: async ({ url, baseUrl }) => {
-      // eslint-disable-next-line no-console
-      console.log("url", url);
-      // eslint-disable-next-line no-console
-      console.log("baseUrl", baseUrl);
-      // if (url.startsWith(baseUrl)) {
-      //   return url;
-      // }
-      // eslint-disable-next-line no-console
-      console.log("redirect root_url", process.env.ROOT_URL);
-      return "https://letterpad.app/posts";
+      if (url.startsWith(baseUrl)) {
+        return url;
+      }
+      return getRootUrl(baseUrl) + "/posts";
     },
     jwt: async ({ token }) => {
       return token;
