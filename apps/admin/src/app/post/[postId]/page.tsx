d0f4e75a6a47b163857359c50ed6b0fee2f89526
Abhishek Saha <abhishek.saha@hometogo.com>
2023-08-09 02:50:54 +0200

    chore: delete unsused Logo component

--- apps/admin/src/app/post/[postId]/page.tsx
+++ apps/admin/src/app/post/[postId]/page.tsx
@@ -3,10 +3,11 @@ import { Post, PostProvider } from "@/components/post";
 
 import { Setting } from "@/__generated__/__types__";
 
-import { useDataProvider } from "../../../context/DataProvider";
+import { useMeAndSettingsContext } from "../../../components/providers/settings";
 
 function EditPost() {
-  const { settings } = useDataProvider();
+  const { settings } = useMeAndSettingsContext();
+  if (!settings) return null;
   return (
     <PostProvider settings={settings}>
       <Post />
