fc765e4dc9d6648d8c90d5f4d19f03ad817e4a80
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2023-09-02 18:53:02 +0200

    Apply css from admin settings to client (#557)

--- apps/client/themes/grid/commons/site-logo.tsx
+++ apps/client/themes/grid/commons/site-logo.tsx
@@ -17,20 +17,18 @@ export const LogoWithTitle = ({
   const logoProps = getLogoWidthAndHeight({ ...logo, title });
   const { className, ...props } = logoProps;
   const hasTitle = typeof title === 'string';
-  const logoExist = !!logo?.src;
   return (
-    <div className={'flex justify-between ' + className}>
+    <div className={'lp-container flex justify-between ' + className}>
       {logo?.src && (
-        <span className="mr-2 flex">
+        <span className="lp-logo mr-2 flex">
           <Image alt={title} {...props} src={logo.src} />
         </span>
       )}
-      {hasTitle &&
-        (logoExist ? (
-          <div className="mt-2 hidden text-md font-bold md:block">{title}</div>
-        ) : (
-          <div className="text-md font-bold">{title}</div>
-        ))}
+      {hasTitle && (
+        <div className="lp-title mt-2 text-lg font-extrabold md:block">
+          {title}
+        </div>
+      )}
     </div>
   );
 };
@@ -40,12 +38,12 @@ interface Props2 extends Logo {
 }
 function getLogoWidthAndHeight({ width, height, src, title = '' }: Props2) {
   if (!width || !height) {
-    return { width: 40, height: 40, src, classname: '' };
+    return { width: 60, height: 60, src, classname: '' };
   }
   const ratio = width / height;
 
   if (ratio === 1) {
-    return { width: 40, height: 40, src, className: 'flex-row items-left ' };
+    return { width: 60, height: 60, src, className: 'flex-row items-left ' };
   }
   if (ratio > 1) {
     const ratio = width / height;
@@ -53,7 +51,7 @@ function getLogoWidthAndHeight({ width, height, src, title = '' }: Props2) {
     if (title.length <= 20) {
       className = ' flex-row items-center';
     }
-    return { width: 140, height: 140 / ratio, src, className };
+    return { width: 80, height: 80 / ratio, src, className };
   }
   if (ratio < 1) {
     const ratio = width / height;
