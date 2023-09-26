85ce267ebb52820adcda175c315aebb6f505fa2b
Abhishek Saha <abhishek.saha@hometogo.com>
2023-09-27 01:54:42 +0200

    fix: author page

--- apps/client/components/fonts/fontPageWrapper.tsx
+++ apps/client/components/fonts/fontPageWrapper.tsx
@@ -1,15 +1,13 @@
-// 'use client';
-import classNames from 'classnames';
-
-// import { fonts } from './fonts';
-
 export const FontPageWrapper = ({
   children,
   primary_font = 'Inter',
   secondary_font = 'PT_Serif',
 }) => {
   return (
-    <div style={{ fontFamily: `'${primary_font.replace(/_/g, ' ')}'` }}>
+    <div
+      style={{ fontFamily: `'${primary_font.replace(/_/g, ' ')}'` }}
+      className="flex min-h-screen flex-col"
+    >
       {children}
     </div>
   );
