3e6ece721497dcca413271624feb8b6ae5902ee3
Abhishek Saha <abhishek.saha@hometogo.com>
2023-09-29 16:43:53 +0200

    fix: creatives

--- apps/admin/src/app/(public)/try-creatives/page.tsx
+++ apps/admin/src/app/(public)/try-creatives/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import {
   BuilderContext,
