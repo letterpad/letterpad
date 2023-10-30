cdfa176151e1bb1d8350f7ebe35f6aa5ecd6d3ee
Abhishek Saha <abhishek.saha@hometogo.com>
2023-10-30 21:34:19 +0530

    chore: skip seed

--- apps/admin/src/graphql/db/seed/seedIfNoSeed.ts
+++ apps/admin/src/graphql/db/seed/seedIfNoSeed.ts
@@ -2,8 +2,10 @@ require("dotenv/config");
 import { prisma } from "@/lib/prisma";
 
 import { seed } from "./seed";
+import { db } from "../../../lib/drizzle";
 
 hasSeed().then((seeded) => {
+  return;
   if (process.env.NODE_ENV === "production") return;
   // eslint-disable-next-line no-console
   console.log("DB URL", process.env.DATABASE_URL);
@@ -24,7 +26,8 @@ hasSeed().then((seeded) => {
 
 async function hasSeed() {
   try {
-    const author = await prisma.author.findFirst();
+    const author = await db.query.Author.findFirst();
+    // const author = await prisma.author.findFirst();
     return author;
   } catch (e) {
     return false;
