af8ac04a01c9ece127d196884951ef200de4d94a
Abhishek Saha <abhishek.saha@hometogo.com>
2023-12-23 22:30:53 +0530

    chore: add ai plugin

--- apps/admin/src/app/post/[postId]/_feature/components/editor.tsx
+++ apps/admin/src/app/post/[postId]/_feature/components/editor.tsx
@@ -1,6 +1,22 @@
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import { createLowlight } from "lowlight";
 import dynamic from "next/dynamic";
-import { FC, Suspense, useEffect, useState } from "react";
+import { FC, Suspense } from "react";
 import { TextBlockPlaceholder } from "ui";
+const lowlight = createLowlight();
+
+lowlight.register("html", html);
+lowlight.register("css", css);
+lowlight.register("js", js);
+lowlight.register("ts", ts);
+
+const Highlight = CodeBlockLowlight.configure({
+  lowlight,
+});
 
 const TinyMceEditor = dynamic(() => import("./tinymce/tinymce"), {
   suspense: true,
