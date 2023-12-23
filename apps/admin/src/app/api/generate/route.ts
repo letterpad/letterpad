6478aa9fb819aa7dafc0dc0bbf4105839146eca4
Abhishek Saha <abhishek.saha@hometogo.com>
2023-12-24 00:42:22 +0530

    chore: add ai route

---
+++ apps/admin/src/app/api/generate/route.ts
@@ -0,0 +1,53 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
+
+export const runtime = "edge";
+
+export async function POST(req: Request): Promise<Response> {
+  // Check if the OPENAI_API_KEY is set, if not return 400
+  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+    return new Response(
+      "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
+      {
+        status: 400,
+      }
+    );
+  }
+
+  let { prompt } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that continues existing text based on context from prior text. " +
+          "Give more weight/priority to the later characters than the beginning ones. " +
+          "Do not write any code even if you are asked to do so. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    n: 1,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
