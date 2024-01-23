aaf32b29969a2ce01bd1407a22ab7b66a0d49239
Abhishek Saha <abhishek.saha@hometogo.com>
2024-01-23 10:33:14 +0100

    chore: add utility hooks

---
+++ apps/client/src/hooks/useOnClickOutisde.ts
@@ -0,0 +1,22 @@
+import { RefObject } from 'react';
+
+import { useEventListener } from './useEventListener';
+
+type Handler = (event: MouseEvent) => void;
+
+export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: Handler,
+  mouseEvent: 'mousedown' | 'mouseup' = 'mousedown'
+): void {
+  useEventListener(mouseEvent, (event) => {
+    const el = ref?.current;
+
+    // Do nothing if clicking ref's element or descendent elements
+    if (!el || el.contains(event.target as Node)) {
+      return;
+    }
+
+    handler(event);
+  });
+}
