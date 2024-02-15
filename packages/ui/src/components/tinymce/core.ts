2fa702a4278a6ecfa9ca314497bb6dcfa9c2c98e
Abhishek Saha <abhishek.saha@hometogo.com>
2024-02-15 22:26:35 +0100

    chore: weird 1mb exceeding problem

--- packages/ui/src/components/tinymce/core.ts
+++ packages/ui/src/components/tinymce/core.ts
@@ -1,14 +1,11 @@
 if (typeof window !== "undefined") {
   require("tinymce/tinymce");
   require("tinymce/models/dom");
-  require("tinymce/themes/silver/theme.js");
   require("tinymce/plugins/lists");
   require("tinymce/plugins/link");
   require("tinymce/plugins/anchor");
-  require("tinymce/plugins/code");
   require("tinymce/plugins/quickbars");
   require("tinymce/plugins/autoresize");
-  require("tinymce/plugins/codesample");
   require("tinymce/icons/default");
   require("tinymce/skins/ui/oxide-dark/skin.min.css");
   require("tinymce/skins/ui/oxide-dark/content.min.css");
