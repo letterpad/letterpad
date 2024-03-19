e8e9ce4ba8fa18d772b6130570c90578e5a99ee9
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2024-03-19 12:45:24 +0100

    Add support for Postgress (#649)
    * chore: add postgress
    
    * chore: fix tags
    
    * chore: add migrations

--- apps/admin/src/components/website_v2/search.tsx
+++ apps/admin/src/components/website_v2/search.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { CiSearch } from "react-icons/ci";
 import { Modal } from "ui";
@@ -67,9 +68,13 @@ export const Search = () => {
             <button
               disabled={loading}
               type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-10"
             >
-              Search
+              {loading ? (
+                <AiOutlineLoading3Quarters className="animate-spin" />
+              ) : (
+                "Search"
+              )}
             </button>
           </div>
         </form>
