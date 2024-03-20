0a1cd94af10849749fd1e94c2645cd5625b1c226
Boopathi Rajaa <boopathi@users.noreply.github.com>
2024-03-20 21:35:42 +0100

    feat: add grid layout to Wavique theme (#657)
    * feat: add grid layout to Wavique theme
    
    * fix: build

--- apps/client/themes/wavique/home_posts.tsx
+++ apps/client/themes/wavique/home_posts.tsx
@@ -3,6 +3,8 @@ import { HomePostsProps } from 'types/pageTypes';
 
 import formatDate from '@/lib/utils/formatDate';
 
+import { IconBook } from '@/components/icons';
+import Image from '@/components/Image';
 import Link from '@/components/Link';
 
 import { Layout } from './layout';
@@ -12,11 +14,13 @@ export const HomePosts: FC<HomePostsProps> = ({ posts }) => {
   const displayPosts = posts.rows.map((it) => {
     const ret = {
       date: it.publishedAt,
+      coverImage: it.cover_image.src,
       title: it.title,
       summary: it.excerpt,
       tags: (it.tags as any).rows.map((it) => it.name),
       slug: it.slug,
       readingTime: it.stats?.reading_time,
+      publishedAt: it.publishedAt,
     };
 
     return ret;
@@ -25,60 +29,96 @@ export const HomePosts: FC<HomePostsProps> = ({ posts }) => {
   return (
     <Layout>
       <div className="divide-y">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            All Posts
-          </h1>
-        </div>
-        <ListLayout displayPosts={displayPosts} />
+        <GridLayout displayPosts={displayPosts} />
       </div>
     </Layout>
   );
 };
 
-function ListLayout({ displayPosts }) {
+function GridLayout({ displayPosts }) {
   return (
-    <ol
-      className="pt-8 pb-8 m-auto max-w-4xl list-decimal ps-6 sm:marker:text-lg"
-      reversed={true}
-    >
+    <ul className="grid grid-cols-1 gap-8 py-12 md:grid-cols-2 xl:grid-cols-3">
       {!displayPosts.length && 'No posts found.'}
       {displayPosts.map(
-        ({ date, title, summary, tags, slug, readingTime }, i) => {
+        (
+          {
+            date,
+            title,
+            summary,
+            tags,
+            slug,
+            readingTime,
+            coverImage,
+            publishedAt,
+          },
+          i
+        ) => {
           return (
-            <PostCard
+            <GridPostCard
               key={slug}
-              {...{ date, title, summary, tags, slug, readingTime }}
+              {...{
+                date,
+                title,
+                summary,
+                tags,
+                slug,
+                readingTime,
+                coverImage,
+                publishedAt,
+              }}
             />
           );
         }
       )}
-    </ol>
+    </ul>
   );
 }
 
-function PostCard({ date, title, tags, slug }) {
+function GridPostCard({
+  date,
+  title,
+  tags,
+  summary,
+  slug,
+  coverImage,
+  readingTime,
+  publishedAt,
+}) {
   return (
-    <li key={slug} className="py-2">
-      <article className="flex flex-col gap-2 sm:text-lg">
-        <div className="flex flex-wrap gap-2">
-          <Link
-            href={`${slug}`}
-            className="underline decoration-primary-500 underline-offset-4 text-primary font-normal hover:bg-gray-100 dark:hover:bg-gray-700"
-          >
-            {title}
-          </Link>
-          <dl className="">
-            <dt className="sr-only">Published on</dt>
-            <dd className="text-gray-500 dark:text-gray-400">
-              <time dateTime={date}>{formatDate(date)}</time>
-            </dd>
-          </dl>
-          {tags.map((tag) => (
-            <Tag key={tag} text={tag} />
-          ))}
+    <li className="group w-full">
+      <Link
+        className="c-card block overflow-hidden rounded-2xl bg-white/[0.7] hover:bg-white dark:bg-white/[0.15] dark:hover:bg-white/[0.05]"
+        href={slug ?? ''}
+      >
+        <div className="relative max-h-4 overflow-hidden pb-60">
+          <span>
+            <Image
+              className="transform transition duration-400 group-hover:scale-[1.02]"
+              src={coverImage ?? ''}
+              fill={true}
+              alt={title}
+              style={{ objectFit: 'cover' }}
+            />
+          </span>
         </div>
-      </article>
+        <div className="h-48 p-4 ">
+          <span className="inline-flex w-full items-center justify-between">
+            <span className="flex items-center gap-1 text-sm">
+              <IconBook />
+              {readingTime} read
+            </span>
+
+            <time dateTime={publishedAt} className="text-sm">
+              {formatDate(publishedAt)}
+            </time>
+          </span>
+
+          <h4 className={'mb-2 mt-2 font-bold leading-5'}>{title}</h4>
+          <p className="text-base tracking-tight text-gray-600 dark:text-gray-300">
+            {summary}
+          </p>
+        </div>
+      </Link>
     </li>
   );
 }
