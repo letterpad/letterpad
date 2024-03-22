3dbb140c82b28d565ae59b7b2943bcbd6b281dc7
Abhishek Saha <abhishek.saha@hometogo.com>
2024-03-22 11:32:13 +0100

    chore: edge config window

--- apps/admin/src/shared/utils.ts
+++ apps/admin/src/shared/utils.ts
@@ -109,6 +109,6 @@ export const beautifyTopic = (str: string) => {
 
 
 export const getEdgeConfigClient = () => {
-  if (typeof window !== "undefined") return (window as any).edgeConfig as Record<string, any>;
+  if (typeof globalThis !== "undefined") return (globalThis as any).edgeConfig as Record<string, any>;
   return null;
 }
