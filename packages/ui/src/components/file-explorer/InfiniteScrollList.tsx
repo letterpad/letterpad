71e82609f7220890b0276bf026d7d047b3211cf2
Abhishek Saha <abhishek.saha@hometogo.com>
2024-03-24 16:21:51 +0100

    chore: fix media loading

--- packages/ui/src/components/file-explorer/InfiniteScrollList.tsx
+++ packages/ui/src/components/file-explorer/InfiniteScrollList.tsx
@@ -1,26 +1,29 @@
+import classNames from "classnames";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 interface IInfiniteScrollListProps {
   data: any;
   count: number;
   loadMore: () => void;
+  className?: string;
 }
 
 export const InfiniteScrollList = ({
   data,
   count,
   loadMore,
+  className
 }: IInfiniteScrollListProps) => {
   return (
-    <div className="grid">
+    <div className={classNames("grid overflow-y-scroll", className)} id="scrollableDiv">
       <InfiniteScroll
-        height={400}
+        className={className}
         dataLength={data.length}
         next={loadMore}
         hasMore={data.length < count}
         loader={null}
       >
-        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6 h-full">
           {data}
         </div>
       </InfiniteScroll>
