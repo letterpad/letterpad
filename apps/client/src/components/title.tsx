dad091a335920682404e29b28ebbafe6890fdc8b
Abhishek Saha <abhishek.saha@hometogo.com>
2024-03-26 15:41:28 +0100

    chore: fix title line-height

--- apps/client/src/components/title.tsx
+++ apps/client/src/components/title.tsx
@@ -10,7 +10,7 @@ export function PageTitle({ children, className }: Props) {
   return (
     <h1
       className={classNames(
-        'py-2 pb-2 text-3xl font-bold leading-6 tracking-tight md:text-4xl md:leading-10',
+        'py-2 pb-2 text-3xl font-bold leading-8 tracking-tight md:text-4xl md:leading-10',
         className
       )}
     >
