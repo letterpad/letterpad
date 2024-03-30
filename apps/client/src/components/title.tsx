7387675bb4199228b182906b8bbb7af8ba56b0b2
Abhishek Saha <abhishek.saha@hometogo.com>
2024-03-31 00:12:31 +0100

    chore: improve cover image

--- apps/client/src/components/title.tsx
+++ apps/client/src/components/title.tsx
@@ -10,7 +10,7 @@ export function PageTitle({ children, className }: Props) {
   return (
     <h1
       className={classNames(
-        'py-2 pb-2 text-3xl font-bold leading-8 tracking-tight md:text-4xl md:leading-10',
+        'py-2 pb-2 text-3xl font-medium leading-9 tracking-tight md:text-5xl md:leading-14 font-serif',
         className
       )}
     >
