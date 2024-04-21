146992f302d3da48cec63d5d0be78aa3f0c598ab
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2024-04-21 22:40:37 +0200

    chore: refactor UI components and layouts (#723)

--- apps/admin/src/app/(public)/messages/registered/page.tsx
+++ apps/admin/src/app/(public)/messages/registered/page.tsx
@@ -1,27 +1,35 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { Button, Result } from "ui";
-
-const Registered = () => {
-  const router = useRouter();
-
-  const doLogin = () => {
-    router.push("/api/auth/signin");
-  };
-  return (
-    <>
-      <Result
-        status="success"
-        title="Registration successful"
-        subTitle="Congrats! You have successfully registered with Letterpad. You will receive an email shortly from us to verify your identity."
-        extra={[
-          <Button key="login" onClick={doLogin}>
-            Login
-          </Button>,
-        ]}
-      />
-    </>
-  );
+export const metadata = {
+  title: "Letterpad - Email Verified",
 };
+
+import { BiRocket } from "react-icons/bi";
+import { BsArrowRight } from "react-icons/bs";
+import { IoSparkles } from "react-icons/io5";
+import { Button } from "ui/isomorphic";
+
+export default function Registered() {
+  return (
+    <form action="/api/auth/signin">
+      <div className="flex flex-col h-full items-center justify-center  bg-gray-50 dark:bg-gray-900 px-4 md:px-6">
+        <div className="max-w-md w-full space-y-6 text-center">
+          <h1 className="md:text-4xl text-xl font-bold text-gray-900 dark:text-gray-50">
+            <BiRocket className="mr-2 inline-block h-8 w-8 text-primary" />
+            Welcome Aboard!
+          </h1>
+          <p className="text-lg text-gray-500 dark:text-gray-400">
+            <IoSparkles className="mr-2 inline-block h-6 w-6 text-primary" />
+            You have been registered successfully.
+          </p>
+          <div className="relative">
+            <Button className="w-full" type="submit" variant={"success"}>
+              <BsArrowRight className="mr-2 inline-block h-5 w-5" />
+              Proceed to Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
 Registered.isMessage = true;
-export default Registered;
