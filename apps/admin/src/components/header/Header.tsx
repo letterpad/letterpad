40b56e8100a3d8bf5f5a065a3262d23f7fad248d
Abhishek Saha <abhishek.saha@hometogo.com>
2024-04-23 00:36:54 +0200

    chore: change header menu

--- apps/admin/src/components/header/Header.tsx
+++ apps/admin/src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 "use client";
 import classNames from "classnames";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
-import { Drawer, ThemeSwitcher } from "ui";
+import { Button, Drawer, ThemeSwitcher } from "ui";
 
 import { ProfileDropdown } from "@/components/profile-dd";
 import { Search } from "@/components/website_v2/search";
@@ -11,9 +12,49 @@ import { Search } from "@/components/website_v2/search";
 // @ts-ignore
 import Logo from "/public/logo/logo-full.png";
 
+const menu = (isLoggedIn: boolean) => [
+  {
+    link: "/login",
+    title: "Login",
+    className: "hidden md:block",
+    visible: !isLoggedIn,
+  },
+  {
+    link: "/register",
+    title: "Register",
+    className: "hidden md:block",
+    visible: !isLoggedIn,
+  },
+  {
+    link: "/resources/monetise",
+    title: "Monetise",
+    className: "hidden md:block",
+    visible: true,
+  },
+  {
+    link: "/resources",
+    title: "Resources",
+    className: "hidden md:block",
+    visible: true,
+  },
+  {
+    link: "/pricing",
+    title: "Pricing",
+    className: "hidden md:block",
+    visible: true,
+  },
+  {
+    link: "/features",
+    title: "Features",
+    className: "hidden md:block text-sky-400",
+    visible: true,
+  },
+];
+
 function Header({ displayBg = true }: { displayBg?: boolean }) {
   const [show, setShow] = useState(false);
-
+  const { data } = useSession();
+  const isLoggedIn = !!data?.user?.id;
   return (
     <header
       className={classNames("z-30 w-full", {
@@ -36,28 +77,31 @@ function Header({ displayBg = true }: { displayBg?: boolean }) {
           </div>
           <nav className="flex grow">
             <ul className="flex grow flex-wrap items-center justify-end gap-6">
+              {menu(isLoggedIn)
+                .filter((item) => item.visible)
+                .map((item) => (
+                  <li className={item.className} key={item.title}>
+                    <Link href={item.link}>{item.title}</Link>
+                  </li>
+                ))}
               <li className="hidden md:block">
-                <Link href="/resources/monetise">Monetise</Link>
-              </li>
-              <li className="hidden md:block">
-                <Link href="/resources">Resources</Link>
-              </li>
-              <li className="hidden md:block">
-                <Link href="/pricing">Pricing</Link>
-              </li>
-
-              <li className="hidden md:block">
-                <Link href="/features">Pro</Link>
-              </li>
-              <li>
                 <Search />
               </li>
               <li>
                 <ThemeSwitcher />
               </li>
-              <li>
-                <ProfileDropdown />
-              </li>
+              {isLoggedIn ? (
+                <li>
+                  <ProfileDropdown />
+                </li>
+              ) : (
+                <Link
+                  href="/login"
+                  className="md:hidden rounded-full border px-3 py-1.5 bg-sky-600 text-sm"
+                >
+                  Get Started
+                </Link>
+              )}
             </ul>
           </nav>
         </div>
@@ -70,18 +114,13 @@ function Header({ displayBg = true }: { displayBg?: boolean }) {
         className="w-64"
       >
         <ul className="flex flex-col grow flex-wrap justify-end items-center gap-4 text-md mt-10">
-          <li>
-            <Link href="/resources/monetise">Monetise</Link>
-          </li>
-          <li>
-            <Link href="/resources">Resources</Link>
-          </li>
-          <li>
-            <Link href="/features">Pro</Link>
-          </li>
-          <li>
-            <Link href="/pricing">Pricing</Link>
-          </li>
+          {menu(isLoggedIn)
+            .filter((item) => item.visible)
+            .map((item) => (
+              <li key={item.title}>
+                <Link href={item.link}>{item.title}</Link>
+              </li>
+            ))}
         </ul>
       </Drawer>
     </header>
