328bb51892cc40eb8e2bf0e3b8c1c9838be6d236
Abhishek Saha <abhishek.saha@hometogo.com>
2024-05-28 17:20:46 +0200

    chore: add source for no source

--- apps/admin/src/app/(public)/login/page.tsx
+++ apps/admin/src/app/(public)/login/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { AuthForm } from "ui/dist/index.mjs";
 
 const Login = () => {
   const params = useSearchParams();
-  const source = params.get("source") || "/";
+  const [source, setSource] = React.useState(params.get("source")!);
+
+  useEffect(() => {
+    if (!source) {
+      setSource(window.location.origin + "/posts");
+    }
+  }, [source]);
+
   return (
     <div className="bg-white dark:bg-gray-900 flex justify-center  flex-1">
       <AuthForm source={source} className="!max-w-lg p-8" view="login" />
