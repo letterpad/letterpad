937752673c8eb6c698b7e7079160add4af8f5a12
Abhishek Saha <abhishek.saha@hometogo.com>
2024-05-29 16:14:17 +0200

    chore: render localhost image as is

--- packages/ui/src/components/image/index.tsx
+++ packages/ui/src/components/image/index.tsx
@@ -7,7 +7,7 @@ export const Image = forwardRef<
 >(({ className, src, alt, loading, ...props }, ref) => {
 
   const root = new URL(process.env.NEXT_PUBLIC_ROOT_URL! || process.env.NEXT_PUBLIC_API_URL!).origin;
-  if (src?.startsWith('/') && process.env.NODE_ENV === 'production') {
+  if (src?.startsWith('/') && !root.includes('localhost')) {
     const fullImgUrl = new URL(src, root).href;
     src =`https://res.cloudinary.com/abhisheksaha/image/fetch/f_auto,q_auto/${fullImgUrl}`;
 
